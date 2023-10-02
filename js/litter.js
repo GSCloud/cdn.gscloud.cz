08ee129daa64eb7e3bb96fe8e33ed64a3de31f6f
Filip Oščádal <f@mxd.cz>
2023-10-02 20:24:17 +0200

    minor update

--- js/litter.js
+++ js/litter.js
@@ -1,24 +1,28 @@
 (function(w, d, undefined) {
-    'use strict';
+  'use strict';
 
-    window.scrollpx = 0;
-    window.scrolled = 0;
+  window.scrollpx = 0;
+  window.scrolled = 0;
+
+  window.addEventListener("load", function() {
 
     // mark the scroll position to globals
     window.onscroll = function() {
-        let scroll = document.body.scrollTop || document.documentElement.scrollTop;
-        let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        let scrolled = (scroll / height) * 100;
+      let scroll = document.body.scrollTop || document.documentElement.scrollTop;
+      let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      let scrolled = (scroll / height) * 100;
 
-        window.scrollpx = scroll; // px scrolled
-        window.scrolled = scrolled; // % scrolled
+      window.scrollpx = scroll; // px scrolled
+      window.scrolled = scrolled; // % scrolled
     };
 
     // flow button click scroll to top
     $('#nav > a:nth-child(5)').click(function() {
-        $('html').animate({scrollTop: 0}, 'slow').delay(50).queue(function () {
-            location.reload()
-        });
+      $('html').animate({scrollTop: 0}, 'slow').delay(50).queue(function () {
+        location.reload()
+      });
     });
+
+  }); // end event handler
 
 })(window, document);
