956e2c2be17e6914baa142bf2c53c5537cf272bf
Filip Oščádal <f@mxd.cz>
2023-10-02 20:37:13 +0200

    minor update

--- js/litter.js
+++ js/litter.js
@@ -5,19 +5,18 @@
   window.scrolled = 0;
 
   window.addEventListener("load", function() {
-
     // mark the scroll position to globals
     window.onscroll = function() {
       let scroll = document.body.scrollTop || document.documentElement.scrollTop;
       let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
       let scrolled = (scroll / height) * 100;
-
       window.scrollpx = scroll; // px scrolled
       window.scrolled = scrolled; // % scrolled
     };
 
     // delayed fn
     setTimeout(function() {
+      console.log("delayed fn from litter.js");
       // flow button click scroll to top
       $('#nav > a:nth-child(5)').click(function () {
         $('html').animate({scrollTop: 0}, 'slow').delay(50).queue(function () {
